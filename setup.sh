b664226b6d9e69bbf95dfddfbed35bef8cf3f244
Ethan Holz <ethan2@vt.edu>
2021-04-13 16:12:43 -0400

    Removed fish

--- setup.sh
+++ setup.sh
@@ -14,20 +14,12 @@ elif [ -x "$(command -v zypper)" ];  then sudo zypper ref && sudo zypper update;
 elif [ -x "$(command -v pacman)" ];  then sudo pacman -Syu; PM='pac';
 else echo "FAILED TO FIND PACKAGE MANAGER"; fi
 
-# the following is only for Ubuntu-based distros
-if [ $PM == 'apt' ]; then apt-add-repository ppa:fish-shell/release-3 && apt update; fi 
-
 
 if [ -x "$(command -v apt)" ]; then sudo apt install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 elif [ -x "$(command -v pacman)" ];  then sudo pacman -S $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
-
-echo "Starting Powerline-Go install"
-go get -u github.com/justjanne/powerline-go
-echo "Finished Powerline-Go install"
-
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -39,23 +31,14 @@ else
 	echo ".config directory does not exist..."
 	mkdir .config
 fi
-
-cd $HOME/.config
-
-if [ -d "fish" ]; then rm -rf fish; fi
-if [ -d "nvim" ]; then rm -rf nvim; fi
-if [ -d ".git" ]; then rm -rf .git; fi
-
-git init
-git remote add pi ubuntu@192.168.50.75:/home/ubuntu/config.git
-git pull pi master
+curl -L git.io/antigen > ~/.config/zsh-plugins/antigen.zsh
 
 git config --global user.email "ethan2@vt.edu"
 git config --gloabl user.name "Ethan Holz"
 git config --global core.editor nvim
 
-FILE1=/usr/local/bin/fish
-FILE2=/usr/bin/fish
+FILE1=/usr/local/bin/zsh
+FILE2=/usr/bin/zsh
 if [ -f "$FILE1" ]; then
 	echo "$FILE1 exists."
 	echo $FILE1 | sudo tee -a /etc/shells
