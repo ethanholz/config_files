d80c4eb4bc16d9ceaf13cf64332b705a98e1abbb
Ethan Holz <ethan2@vt.edu>
2021-04-26 12:59:34 -0400

    Added require_plugin, treesitter, theme

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -4,7 +4,9 @@ require('lspconf')
 -- Map leader config
 vim.g.mapleader=','
 vim.wo.relativenumber=true
--- vim.o.termguicolors=true
+-- Set colorscheme with colorbuddy
+require('colorbuddy').colorscheme('gruvbuddy')
+--Set Bufferline
 require('bufferline').setup{}
 -- Set Lualine
 require('lualine').setup{options={theme='gruvbox_material'}}
@@ -16,5 +18,19 @@ local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
 map('n', '<Leader>t', '<CMD>lua require("FTerm").toggle()<CR>', opts)
 map('t', '<Leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+--Bufferline Keymaps
 map('n', '<Leader>[', ':BufferLineCyclePrev<CR>', {noremap=true, silent=true})
 map('n', '<Leader>]', ':BufferLineCycleNext<CR>', {noremap=true, silent=true})
+-- Treesitter Consistent Syntax Highlighting and indent
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    indent = {
+	    enable = true,
+    },
+    custom_captures = {
+      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+      ["foo.bar"] = "Identifier",
+    },
+  },
+}
--- nvim-lua/lua/plugins.lua
+++ nvim-lua/lua/plugins.lua
@@ -8,10 +8,29 @@ if fn.empty(fn.glob(install_path)) > 0 then
   execute 'packadd packer.nvim'
 end
 
+local function require_plugin(plugin)
+    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
+
+    local plugin_path = plugin_prefix .. plugin .. "/"
+    --	print('test '..plugin_path)
+    local ok, err, code = os.rename(plugin_path, plugin_path)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    --	print(ok, err, code)
+    if ok then
+        vim.cmd("packadd " .. plugin)
+    end
+    return ok, err, code
+end
+
 require('packer').startup(function(use)
 	use "wbthomason/packer.nvim"
-	use "neovim/nvim-lspconfig"
-	use 'nvim-lua/completion-nvim'
+	use {"neovim/nvim-lspconfig", opt=true}
+	use {'nvim-lua/completion-nvim', opt=true}
 	use {
 	  'hoob3rt/lualine.nvim',
 	  requires = {'kyazdani42/nvim-web-devicons', opt = true}
@@ -23,6 +42,11 @@ require('packer').startup(function(use)
 	    end
 	}
 	use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
-
+	use {'tjdevries/gruvbuddy.nvim', requires={'tjdevries/colorbuddy.vim', opt=true}}
+	-- Treesitter
+	use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+	require_plugin("nvim-treesitter")
+	require_plugin("completion-nvim")
+	require_plugin("nvim-lspconfig")
 end)
 
