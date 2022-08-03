b162c5df27ba5fdc93580a58837353ae865bf92d
Ethan Holz <iplaybassx2@gmail.com>
2022-08-02 22:02:48 -0500

    The awesome update

---
+++ awesome/.config/awesome/configuration/apps.lua
@@ -0,0 +1,14 @@
+return {
+	-- List of apps to start once on start-up
+	run_on_start_up = {
+		[[nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"]],
+		"picom -b --dbus",
+		"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)", -- credential manager
+		"flameshot",
+		"steam -silent",
+		-- Add applications that need to be killed between reloads
+		-- to avoid multipled instances, inside the awspawn script
+		"~/.config/awesome/configuration/awspawn", -- Spawn "dirty" apps that can linger between sessions
+		"redshift-gtk",
+	},
+}
---
+++ awesome/.config/awesome/theme/init.lua
@@ -0,0 +1,5 @@
+local dir = os.getenv("XDG_CONFIG_HOME") .. "/awesome/theme/icons"
+
+return {
+	discord = dir .. "/discord.svg",
+}
