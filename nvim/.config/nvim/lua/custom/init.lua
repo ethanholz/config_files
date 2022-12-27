f8c3c3593c9a9c0d16548e93965ab1d6f3e5e41b
Ethan Holz <iplaybassx2@gmail.com>
2022-12-27 10:11:51 -0700

    fix: Fixed error in plugin loading

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,14 +1,11 @@
--- packer.nvim
-local packer_ok, _ = pcall(require, "packer")
-if packer_ok then
-    require("custom.packer-plugins")
-end
+-- -- packer.nvim
+-- local packer_ok, _ = pcall(require, "packer")
+-- if packer_ok then
+--     require("custom.packer-plugins")
+-- end
 
 -- lazy.nvim
-local lazy_ok, _ = pcall(require, "lazy")
-if lazy_ok and not packer_ok then
-    require("custom.plugins")
-end
+require("custom.plugins")
 
 -- Set variables
 require("custom.set")
