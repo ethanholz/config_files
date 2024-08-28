68623096f24d654b7754e0150d178863c7273692
Ethan Holz <ethan.holz@thoriumworks.com>
2024-08-28 18:41:06 -0500

    remove old configs

--- awesome/.config/awesome/custom/defaults.lua
+++
@@ -1,8 +0,0 @@
--- Hold default applications to handle more consistent mappings
-local defaults = {
-	browser = "firefox",
-	suspend = "slock systemctl suspend -i",
-	work_apps = { slack = "flatpak run com.slack.Slack", thunderbird = "thunderbird" },
-}
-
-return defaults
--- awesome/.config/awesome/theme/init.lua
+++
@@ -1,5 +0,0 @@
-local dir = os.getenv("XDG_CONFIG_HOME") .. "/awesome/theme/icons"
-
-return {
-	discord = dir .. "/discord.svg",
-}
