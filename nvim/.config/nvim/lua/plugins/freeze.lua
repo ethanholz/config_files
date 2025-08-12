c7b9e18ba81ef434e6371ddaf5ee676900062758
Ethan Holz <ethan.holz@thoriumworks.com>
2025-08-12 02:40:25 -0600

    refactor: switch to a different freeze.nvim

--- nvim/.config/nvim/lua/plugins/freeze.lua
+++ nvim/.config/nvim/lua/plugins/freeze.lua
@@ -1,10 +1,13 @@
 return {
-	{
-		"ethanholz/freeze.nvim",
-		-- dir = "~/Documents/code/personal/freeze",
-		config = true,
-		opts = {
-			theme = "rose-pine-moon",
-		},
-	},
+    {
+        "charm-and-friends/freeze.nvim",
+        opts = {
+            command = "freeze",
+            open = true,
+            output = function()
+                return "./" .. os.date("%Y-%m-%d") .. "_freeze.png"
+            end,
+            theme = "rose-pine-moon",
+        },
+    },
 }
